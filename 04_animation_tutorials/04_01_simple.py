dd240b48a90e776f2cf428a80c48eb9f6490b083
Francisco Jesús Rubio Reales <fj.rubio@dosfarma.com>
2021-06-06 13:15:24 +0200

    from python 2x to 3x and clean empty lines

--- 04_animation_tutorials/04_01_simple.py
+++ 04_animation_tutorials/04_01_simple.py
@@ -35,7 +35,7 @@ class SimpleAnimation(Sprite):
         self.image = frames[0]  # just to prevent some errors
         self.rect = self.image.get_rect()    # same here
         self.playing = 0
-        
+
     def update(self, *args):
         if self.playing:    # only update the animation if it is playing
             self.current += 1
@@ -44,17 +44,17 @@ class SimpleAnimation(Sprite):
             self.image = self.frames[self.current]
             # only needed if size changes within the animation
             self.rect = self.image.get_rect(center=self.rect.center)
-            
+
     def start(self):
         self.current = 0
         self.playing = True
-        
+
     def stop(self):
         self.playing = False
-        
+
     def pause(self):
         self.playing = False
-        
+
     def resume(self):
         self.playing = True
 
@@ -64,7 +64,7 @@ def get_sequence(frames_names, sequence, optimize=True):
     frames = []
     global cache
     for name in frames_names:
-        if not cache.has_key(name): # check if it has benn loaded already
+        if name not in cache: # check if it has benn loaded already
             image = pygame.image.load(name) # not optimized
             if optimize:
                 if image.get_alpha() is not None:
@@ -72,7 +72,7 @@ def get_sequence(frames_names, sequence, optimize=True):
                 else:
                     image = image.convert()
             cache[name] = image
-            
+
         # constructs a sequence of frames equal to frames_names
         frames.append(cache[name]) 
     frames2 = []
@@ -94,22 +94,22 @@ def get_names_list(basename, ext, num, num_digits=1, offset=0):
 
 # define a main function
 def main():
-    
+
     # initialize the pygame module
     pygame.init()
-    
+
     # load and set the logo
     logo = pygame.image.load(os.path.normpath("data/logo32x32.png"))
     pygame.display.set_icon(logo)
     caption_str = os.path.split(__file__)[1]+"  keys: a/q: change fps, space: pause/resume, r: start/stop "
     pygame.display.set_caption(caption_str)
-    
+
     # create a surface on screen that has the size of 800 x 600
     screen = pygame.display.set_mode((800,600))
-    
+
     # define a variable to control the main loop
     running = True
-    
+
     # generate a list of names    
     image_names = get_names_list(os.path.normpath("data/ball"), "png", 20, 2, 1)
     # generate a sequence, here simply 0,1,2,3...
@@ -120,7 +120,7 @@ def main():
     anim = SimpleAnimation(frames)
     anim.rect.topleft = (400-75,300-75) 
     anim.start()
-    
+
     # use a clock to fix the fps of the main loop
     clock = pygame.time.Clock()
     fps = 20
@@ -163,8 +163,8 @@ def main():
         dirty_rect = screen.blit(anim.image, anim.rect)
         # update screen
         pygame.display.update(dirty_rect)
-    
-    
+
+
 # run the main function only if this module is executed as the main script
 # (if you import this as a module then nothing is executed)
 if __name__=="__main__":
